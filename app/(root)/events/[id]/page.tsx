a9a59e90cf028f9e16c7bbb07ed6ee60d65af138
Anirudh <anirudh.das2903@outlook.in>
2024-04-20 01:19:03 +0530

    added a my profile page along with sections for displaying bought tickets and organized events
    Started implementation of stripe integraton in website

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,14 +1,25 @@
-import { getEventById } from '@/lib/actions/event.actions';
+import { getEventById, getRelatedEventsByCategory } from '@/lib/actions/event.actions';
 import { SearchParamProps } from '@/types';
 import Calendar from '../../../../public /assets/icons/calendar.svg';
 import Location from '../../../../public /assets/icons/location.svg';
 import Image from 'next/image';
 import React from 'react';
 import { formatDateTime } from '@/lib/utils';
+import Collection from '@/components/shared/Collection';
+import CheckoutButton from '@/components/shared/CheckoutButton';
 
-const EventDetails = async ({params: {id}}: SearchParamProps) => {
+const EventDetails = async ({params: {id} , searchParams}: SearchParamProps) => {
+
   const event = await getEventById(id);
+
+  const relatedEvents = await getRelatedEventsByCategory({
+    categoryId: event.category._id,
+    eventId: event._id,
+    page: searchParams.page as string,
+  });
+
   return (
+  <>
     <section className='flex justify-center bg-primary-50 bg-dotted-pattern bg-contain'>
       <div className='grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl'>
         <Image src={event.imageUrl} alt='hero image' width={1000} height={1000} className='h-full min-h-[300px] object-center object-cover'/>
@@ -17,7 +28,7 @@ const EventDetails = async ({params: {id}}: SearchParamProps) => {
             <h2 className='h2-bold'>{event.title}</h2>
             <div className='flex flex-col gap-3 sm:flex-row sm:items-center'>
               <div className='flex gap-3'>
-                <p className='p-bold-20 rounded-full px-5 py-2 text-green-700 bg-green-500/10'>{event.isFree ? 'FREE' : `₹${event.price}`}</p>
+                <p className='p-bold-20 rounded-full px-5 py-2 text-green-700 bg-green-500/10'>{event.isFree ? 'FREE' : `₹ ${event.price}`}</p>
                 <p className='p-medium-16 rounded-full px-4 py-2.5 text-grey-500 bg-grey-500/10'>{event.category.name}</p>
               </div>
                 <p className='p-medium-18 ml-2 mt-2 sm:mt-0'>
@@ -26,7 +37,11 @@ const EventDetails = async ({params: {id}}: SearchParamProps) => {
                 </p>
             </div>
           </div>
+
           {/* CHECKOUT BUTTON */}
+
+          <CheckoutButton event={event} />
+
           <div className='flex flex-col gap-5'>
             <div className='flex gap-2 md:gap-3'>
               <Image src={Calendar} alt='calendar' width={32} height={32}/>
@@ -53,6 +68,20 @@ const EventDetails = async ({params: {id}}: SearchParamProps) => {
         </div>
       </div>
     </section>
+
+    {/* EVENTS WITH THE SAME CATEGORY */}
+    <section className='wrapper my-8 flex flex-col gap-8 md:gap-12'>
+      <h2 className='h2-bold'>Related Events</h2>
+      <Collection
+          data={relatedEvents?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubText="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={1}
+          totalPages={2} />
+    </section>
+  </>
   )
 }
 
