f50ea430decde1504c4064a54fceb8156b6fe8eb
Anirudh <anirudh.das2903@outlook.in>
2025-02-23 17:56:14 +0530

    updates to clerk api #2

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,35 +1,43 @@
 import Image from "next/image";
 import Link from "next/link";
 import Logo from '../../public/assets/images/logo.png';
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { useAuth, UserButton } from "@clerk/nextjs";
 import { Button } from "../ui/button";
 import NavItems from "./NavItems";
 import MobileNav from "./MobileNav";
 
 const Header = () => {
+
+  const { isSignedIn } = useAuth();
   return (
     <header className="w-full border-b">
         <div className="wrapper flex items-center justify-between">
             <Link href="/" className="w-40"><Image src={Logo}
              alt="Orchestrate Logo" style={{width: '160px',height: 'auto',}}/></Link>
 
-            <SignedIn>
-              <nav className="md:flex-between max-md:hidden w-full max-w-xs">
-                <NavItems />
-              </nav>
-            </SignedIn>
-
+                {isSignedIn ? (
+                    <nav className="md:flex-between max-md:hidden w-full max-w-xs">
+                        <NavItems />
+                    </nav>
+                    ) : (<></>)
+                }
 
              <div className="flex w-32 justify-end gap-3">
-                <SignedIn>
-                  <UserButton/>
-                  <MobileNav />
-                </SignedIn>
-                <SignedOut>
-                    <Button asChild className="rounded-full" size="lg">
-                        <Link href="/sign-in">Login</Link>
-                    </Button>
-                </SignedOut>
+                 {isSignedIn ? (
+                     <>
+                         <UserButton/>
+                         <MobileNav />
+                     </>
+                     ) : (<></>)
+                 }
+                 {!isSignedIn ? (
+                     <>
+                        <Button asChild className="rounded-full" size="lg">
+                            <Link href="/sign-in">Login</Link>
+                        </Button>
+                     </>
+                 ) : (<></>)
+                 }
              </div>
         </div>
     </header>
