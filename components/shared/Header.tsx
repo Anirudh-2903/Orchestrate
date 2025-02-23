29bda254fdd1ee5c5c25a04e6963bc2e70807908
Anirudh <anirudh.das2903@outlook.in>
2025-02-23 18:11:52 +0530

    updates to clerk api #3

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,27 +1,33 @@
 import Image from "next/image";
 import Link from "next/link";
 import Logo from '../../public/assets/images/logo.png';
-import { useAuth, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import {SignedIn, SignedOut, useAuth, UserButton} from "@clerk/nextjs";
 import { Button } from "../ui/button";
 import NavItems from "./NavItems";
 import MobileNav from "./MobileNav";
+import {useRouter} from "next/navigation";
+import {useEffect} from "react";
 
 const Header = () => {
+    const { isSignedIn } = useAuth();
+    const router = useRouter();
 
-  const { isSignedIn } = useAuth();
-  return (
+    useEffect(() => {
+        if (isSignedIn === false) {
+            router.refresh(); // Refresh the page when user signs out
+        }
+    }, [isSignedIn, router]);
+
+    return (
     <header className="w-full border-b">
         <div className="wrapper flex items-center justify-between">
             <Link href="/" className="w-40"><Image src={Logo}
              alt="Orchestrate Logo" style={{width: '160px',height: 'auto',}}/></Link>
 
             <SignedIn>
-                {isSignedIn ? (
-                    <nav className="md:flex-between max-md:hidden w-full max-w-xs">
-                        <NavItems />
-                    </nav>
-                    ) : (<></>)
-                }
+              <nav className="md:flex-between max-md:hidden w-full max-w-xs">
+                <NavItems />
+              </nav>
             </SignedIn>
 
 
